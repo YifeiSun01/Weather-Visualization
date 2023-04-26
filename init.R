4e21829d7af3679cbd8d3f91135067438cb70ce4
YifeiSun01 <97551279+YifeiSun01@users.noreply.github.com>
2023-04-26 00:07:01 -0400

    Update init.R

--- init.R
+++ init.R
@@ -5,7 +5,7 @@
 
 my_packages = c("shiny", "shinythemes", "rgl", "DT", "dplyr", "leaflet", "gt", "scales", "hash", "plotly",
               "chron","ggplot2","viridis","scales","tidyverse","cowplot","extrafont","ggforce","comprehenr",
-               "stringr","LaplacesDemon","data.table","lutz","lubridate")
+               "stringr","LaplacesDemon","data.table","lutz","lubridate","geomtextpath")
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
